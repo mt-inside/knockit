8d7358227570dc659e72c14b577019472b56a2e1
matt <matt>
2012-02-25 21:12:43 +0000

    Settings window.

---
+++ knockit/WindowSettings.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using NAudio.Wave;
+
+namespace knockit
+{
+    /// <summary>
+    /// Interaction logic for WindowSettings.xaml
+    /// </summary>
+    public partial class WindowSettings : Window
+    {
+        public WindowSettings()
+        {
+            InitializeComponent();
+
+
+            for (int i = 0; i < WaveIn.DeviceCount; ++i)
+            {
+                var caps = WaveIn.GetCapabilities(i);
+                listBox1.Items.Add(String.Format("name {0} channels {1}", caps.ProductName, caps.Channels));
+            }
+        }
+    }
+}
