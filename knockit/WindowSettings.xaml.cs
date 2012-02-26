255b5aca48411076c4d3a5eb1228772c88a11f40
matt <matt>
2012-02-26 11:41:57 +0000

    More settings window functionality.

--- knockit/WindowSettings.xaml.cs
+++ knockit/WindowSettings.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 using NAudio.Wave;
 
 namespace knockit
@@ -9,15 +10,57 @@ namespace knockit
     /// </summary>
     public partial class WindowSettings : Window
     {
-        public WindowSettings()
+        private readonly WaveIn _waveIn;
+        private readonly WaveOut _waveOut;
+
+        public WindowSettings(WaveIn waveIn, WaveOut waveOut)
         {
+            _waveIn = waveIn;
+            _waveOut = waveOut;
+
             InitializeComponent();
 
 
             for (int i = 0; i < WaveIn.DeviceCount; ++i)
             {
                 var caps = WaveIn.GetCapabilities(i);
-                listBox1.Items.Add(String.Format("name {0} channels {1}", caps.ProductName, caps.Channels));
+                listBoxIn.Items.Add(String.Format("{0} ({1})", caps.ProductName, caps.Channels));
+            }
+
+            for (int i = 0; i < WaveOut.DeviceCount; ++i)
+            {
+                var caps = WaveOut.GetCapabilities(i);
+                listBoxOut.Items.Add(String.Format("{0} ({1})", caps.ProductName, caps.Channels));
+            }
+        }
+
+
+        private void listBoxIn_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var me = sender as ListBox;
+            if (me != null && me.SelectedIndex >= 0)
+            {
+                /* TODO: don't think this is working */
+                _waveIn.StopRecording();
+                _waveIn.DeviceNumber = me.SelectedIndex;
+                _waveIn.StartRecording();
+            }
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void listBoxOut_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var me = sender as ListBox;
+            if (me != null && me.SelectedIndex >= 0)
+            {
+                /* TODO: don't think this is working */
+                _waveOut.Stop();
+                _waveOut.DeviceNumber = me.SelectedIndex;
+                _waveOut.Play();
             }
         }
     }
