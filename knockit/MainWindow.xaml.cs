2ff18fa89b1ca5658de798df3fac4fcd66c7c433
U-magrathea\user <user@magrathea.(none)>
2012-02-27 23:35:38 +0000

    General tidying up.

--- knockit/MainWindow.xaml.cs
+++ knockit/MainWindow.xaml.cs
@@ -12,49 +12,67 @@ namespace knockit
     public partial class MainWindow : Window
     {
         private const int c_SampleRate = 48000;
-        private const int c_MaxFrequency = c_SampleRate/2;
         private WaveIn m_WaveIn;
         private Recorder m_Recorder = new Recorder(c_SampleRate);
         private WaveOut m_WaveOut;
         private readonly BandPass2 _bandPassProvider;
 
-        public MainWindow()
+        public MainWindow(string[] args)
         {
             InitializeComponent();
+
+            IWaveProvider waveIn;
 
             polygonSpectrumControl1.Recorder = m_Recorder;
 
             m_Recorder.NewPrimaryFrequencyEvent += m_Recorder_NewPrimaryFrequencyEvent;
 
-            m_WaveIn = new WaveIn
+            if (args.Length == 1) /* TODO: button for this */
             {
-                DeviceNumber = 0,
-                WaveFormat = new WaveFormat(m_Recorder.SampleRate, 16, 1)
-            };
+                WaveStream readerStream = new WaveFileReader(args[0]);
+                if (readerStream.WaveFormat.Encoding != WaveFormatEncoding.Pcm)
+                {
+                    readerStream = WaveFormatConversionStream.CreatePcmStream(readerStream);
+                    readerStream = new BlockAlignReductionStream(readerStream);
+                }
+
+                if (readerStream.WaveFormat.BitsPerSample != 16)
+                {
+                    var format = new WaveFormat(readerStream.WaveFormat.SampleRate, 16, readerStream.WaveFormat.Channels);
+                    readerStream = new WaveFormatConversionStream(format, readerStream);
+                }
+                waveIn = new LoopingStream(new WaveChannel32(readerStream));
+            }
+            else
+            {
+                m_WaveIn = new WaveIn
+                {
+                    DeviceNumber = 2,
+                    WaveFormat = new WaveFormat(m_Recorder.SampleRate, 16, 1)
+                };
+                waveIn = new WaveInProvider(m_WaveIn);
+                m_WaveIn.StartRecording();
+            }
             
-            IWaveProvider waveInWaveProvider = new WaveInProvider(m_WaveIn);
-            ISampleProvider sampleChannel = new SampleChannel(waveInWaveProvider);
+            ISampleProvider sampleChannel = new SampleChannel(waveIn); //TODO: when we're not using recorder any more, this stay as a wave provider, I think
             _bandPassProvider = new BandPass2(sampleChannel);
             NotifyingSampleProvider sampleStream = new NotifyingSampleProvider(_bandPassProvider);
-
-            sampleStream.Sample +=sampleStream_Sample;
-            m_WaveIn.StartRecording();
+            sampleStream.Sample += sampleStream_Sample; //TODO: want to get away from using the Recorder, really
 
             m_WaveOut = new WaveOut
-                            {
-                                Volume = 0
-                            };
+            {
+                Volume = 1
+            };
             m_WaveOut.Init(new SampleToWaveProvider(sampleStream));
             m_WaveOut.Play();
             // TODO: dispose of these, close, etc
 
             sliderMinFreq.ValueChanged += SliderMinFreqValueChanged;
             sliderMaxFreq.ValueChanged += SliderMaxFreqValueChanged;
-            SliderMinFreqValueChanged(sliderMinFreq, new RoutedPropertyChangedEventArgs<double>(sliderMinFreq.Minimum,sliderMinFreq.Minimum));
-            SliderMaxFreqValueChanged(sliderMaxFreq, new RoutedPropertyChangedEventArgs<double>(sliderMaxFreq.Maximum,sliderMaxFreq.Maximum));
-
             textBoxPistonDiameter.TextChanged += textBoxPistonDiameter_TextChanged;
-            textBoxEpsilon.TextChanged += textBoxPistonDiameter_TextChanged;
+            textBoxEpsilon.TextChanged += textBoxEpsilon_TextChanged;
+
+            UpdateShit();
         }
 
         private void sampleStream_Sample(object sender, SampleEventArgs e)
@@ -87,21 +105,34 @@ namespace knockit
 
         private void textBoxPistonDiameter_TextChanged(object sender, TextChangedEventArgs e)
         {
-            const float c_KnockFreq1mm = 572200; /* Frequency of knock in a 1mm piston. http://www.phormula.co.uk/KnockCalculator.aspx */
-            double diameter, knockFreq, eFreq;
+            UpdateShit();
+        }
 
-            if (!Double.TryParse(textBoxEpsilon.Text, out eFreq) || eFreq == 0) eFreq = 500; //TODO to const and use for init
+        private void textBoxEpsilon_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateShit();
+        }
 
-            if (Double.TryParse(textBoxPistonDiameter.Text, out diameter) && diameter != 0)
+        private void UpdateShit()
+        {
+            //if (IsInitialized)
             {
-                knockFreq = c_KnockFreq1mm/diameter; //TODO raise event
-                labelKnockFreq.Content = Math.Round(knockFreq/1000,3) + "kHz";
-                sliderMinFreq.Value = knockFreq - eFreq;
-                sliderMaxFreq.Value = knockFreq + eFreq;
-            }
-            else
-            {
-                labelKnockFreq.Content = "--kHz";
+                const float c_KnockFreq1mm = 572200; /* Frequency of knock in a 1mm piston. http://www.phormula.co.uk/KnockCalculator.aspx */
+                double diameter, knockFreq, eFreq;
+    
+                if (!Double.TryParse(textBoxEpsilon.Text, out eFreq) || eFreq == 0) eFreq = 500; //TODO to const and use for init
+    
+                if (Double.TryParse(textBoxPistonDiameter.Text, out diameter) && diameter != 0)
+                {
+                    knockFreq = c_KnockFreq1mm/diameter; //TODO raise event
+                    labelKnockFreq.Content = Math.Round(knockFreq/1000,3) + "kHz";
+                    sliderMinFreq.Value = knockFreq - eFreq;
+                    sliderMaxFreq.Value = knockFreq + eFreq;
+                }
+                else
+                {
+                    labelKnockFreq.Content = "--kHz";
+                }
             }
         }
 
